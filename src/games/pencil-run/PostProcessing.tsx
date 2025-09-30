b3ec981ea77a3d1fb2d7de15beafe5390e030f07
Vishnu <vishnu@hexrfactory.com>
2025-09-30 16:36:01 +0530

    post processing updated

--- src/games/pencil-run/PostProcessing.tsx
+++ src/games/pencil-run/PostProcessing.tsx
@@ -1,6 +1,14 @@
 import { Environment } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { SRGBColorSpace, TextureLoader } from "three";
+// import {
+//     EffectComposer,
+//     Scanline,
+//     Sepia,
+//     Vignette,
+// } from "@react-three/postprocessing";
+// import { BlendFunction } from "postprocessing";
+
 import bg from "./assets/sky_27_2k.webp";
 
 const PostProcessing = () => {
@@ -9,7 +17,23 @@ const PostProcessing = () => {
 
     return (
         <>
+            {/* Background Environment */}
             <Environment map={texture} background />
+
+            {/* Postprocessing pipeline */}
+            {/* <EffectComposer>
+                <Sepia intensity={1.0} blendFunction={BlendFunction.NORMAL} />
+                <Vignette
+                    offset={0.5}
+                    darkness={0.5}
+                    eskil={false}
+                    blendFunction={BlendFunction.NORMAL}
+                />
+                <Scanline
+                    blendFunction={BlendFunction.OVERLAY}
+                    density={1}
+                />
+            </EffectComposer> */}
         </>
     );
 };
